c024ba48d9c1705508e0c82b9f6a6613f031204f
Samuel Neff <sam@serndesign.com>
2024-05-14 18:46:15 -0400

    Radiux dialog works but not integrated; not moving to it now, but it works.

--- packages/browse-no-mui-icons/src/BrowseDialog.tsx
+++ packages/browse-no-mui-icons/src/BrowseDialog.tsx
@@ -1,11 +1,12 @@
-import { MouseEvent, useCallback, useState } from 'react';
-import { startCase } from 'lodash';
-import { CheckFilledIcon, CloseFilledIcon } from 'no-mui-icons-filled';
-import { wait } from 'utikity';
+// import { MouseEvent, useCallback, useState } from 'react';
+// import { startCase } from 'lodash';
+// import { CheckFilledIcon } from 'no-mui-icons-filled';
+// import { wait } from 'utikity';
 
-import { StyleName, iconBaseName, iconSetByName, styleNames } from './iconInfo';
+// import { StyleName, iconBaseName, iconSetByName } from './iconInfo';
 
 import styles from './BrowseDialog.module.scss';
+import { Button, Dialog } from '@radix-ui/themes';
 
 export interface BrowseDialogProps {
   selectedIcon: string;
@@ -17,108 +18,132 @@ export function BrowseDialog({
   onCloseDialog,
 }: BrowseDialogProps) {
 
-  const handleBackgroundClick = useCallback(
-    (event: MouseEvent<HTMLDivElement>) => {
-      event.stopPropagation();
-    },
-    []
-  );
+  // const handleBackgroundClick = useCallback(
+  //   (event: MouseEvent<HTMLDivElement>) => {
+  //     event.stopPropagation();
+  //   },
+  //   []
+  // );
 
-  if (!selectedIcon) {
-    return null;
-  }
+  // if (!selectedIcon) {
+  //   return null;
+  // }
 
   return (
-    <div
-      className={styles.browseDialogBlocker}
-      onClick={onCloseDialog}
-    >
-      <div
-        className={styles.browseDialogContainer}
-        onClick={handleBackgroundClick}
-      >
-        <div
-          className={styles.closeButtonContainer}
-          onClick={onCloseDialog}
-        >
-          <CloseFilledIcon className={styles.closeButtonIcon} />
-        </div>
-
-        <div className={styles.browseDialogInstructions}>
-          Click to copy to clipboard.
-        </div>
-        {
-          styleNames.map(
-            styleName => (
-              <BrowseDetailRow
-                selectedIcon={selectedIcon}
-                styleName={styleName}
-              />
-            )
-          )
+    <Dialog.Root
+      open={Boolean(selectedIcon)}
+      onOpenChange={open => {
+        if (!open) {
+          onCloseDialog();
         }
-      </div>
-    </div>
-  );
-}
-
-interface BrowseDetailRowProps {
-  selectedIcon: string;
-  styleName: StyleName;
-}
-
-function BrowseDetailRow({
-  selectedIcon,
-  styleName,
-}: BrowseDetailRowProps) {
-
-  const [ copiedToClipboard, setCopiedToClipboard ] = useState(false);
-
-  const selectedIconBaseName = iconBaseName(selectedIcon);
-  const styleNamePart = startCase(styleName).replace(/ /g, '');
-  const iconFunctionName = `${ selectedIconBaseName }${ styleNamePart }Icon`;
-  const Icon = iconSetByName[ styleName ][ iconFunctionName ];
-
-
-  const handleClick = useCallback(
-    async (event: MouseEvent<HTMLDivElement>) => {
-      event.stopPropagation();
-      await navigator.clipboard.writeText(iconFunctionName);
-      setCopiedToClipboard(true);
-      await wait(10000);
-      setCopiedToClipboard(false);
-    },
-    [
-      iconFunctionName
-    ]
-  );
-
-  return (
-    <div
-      key={styleName}
-      className={styles.browseDetailRow}
-      onClick={handleClick}
-    >
-      <Icon className={styles.browseDetailIcon} />
-      <span className={styles.browseDetailBaseName}>
-        {selectedIconBaseName}
-      </span>
-      <span className={styles.browseDetailStyleName}>
-        {styleNamePart}
-      </span>
-      <span className={styles.browseDetailIconSuffix}>
-        Icon
-      </span>
-
-      {
-        copiedToClipboard
-          ? (
-            <div className={styles.browseDetailCopiedContainer}>
-              <CheckFilledIcon className={styles.browseDetailCopiedIcon} />
-              Copied to clipboard
-            </div>
-          ) : null
       }
-    </div>
+      }
+    >
+      <Dialog.Content>
+        <Dialog.Title>{selectedIcon}</Dialog.Title>
+
+        <div className={styles.browseDetailTable}>
+          More content here...
+        </div>
+
+        <Dialog.Close>
+          <Button onClick={onCloseDialog}>
+            Done
+          </Button>
+        </Dialog.Close>
+      </Dialog.Content>
+    </Dialog.Root>
+
+    // <div
+    //   className={styles.browseDialogBlocker}
+    //   onClick={onCloseDialog}
+    // >
+    //   <div
+    //     className={styles.browseDialogContainer}
+    //     onClick={handleBackgroundClick}
+    //   >
+    //     <div
+    //       className={styles.closeButtonContainer}
+    //       onClick={onCloseDialog}
+    //     >
+    //       <CloseFilledIcon className={styles.closeButtonIcon} />
+    //     </div>
+
+    //     <div className={styles.browseDialogInstructions}>
+    //       Click to copy to clipboard.
+    //     </div>
+    //     {
+    //       styleNames.map(
+    //         styleName => (
+    //           <BrowseDetailRow
+    //             selectedIcon={selectedIcon}
+    //             styleName={styleName}
+    //           />
+    //         )
+    //       )
+    //     }
+    //   </div>
+    // </div>
   );
 }
+
+// interface BrowseDetailRowProps {
+//   selectedIcon: string;
+//   styleName: StyleName;
+// }
+
+// function BrowseDetailRow({
+//   selectedIcon,
+//   styleName,
+// }: BrowseDetailRowProps) {
+
+//   const [ copiedToClipboard, setCopiedToClipboard ] = useState(false);
+
+//   const selectedIconBaseName = iconBaseName(selectedIcon);
+//   const styleNamePart = startCase(styleName).replace(/ /g, '');
+//   const iconFunctionName = `${ selectedIconBaseName }${ styleNamePart }Icon`;
+//   const Icon = iconSetByName[ styleName ][ iconFunctionName ];
+
+
+//   const handleClick = useCallback(
+//     async (event: MouseEvent<HTMLDivElement>) => {
+//       event.stopPropagation();
+//       await navigator.clipboard.writeText(iconFunctionName);
+//       setCopiedToClipboard(true);
+//       await wait(10000);
+//       setCopiedToClipboard(false);
+//     },
+//     [
+//       iconFunctionName
+//     ]
+//   );
+
+//   return (
+//     <div
+//       key={styleName}
+//       className={styles.browseDetailRow}
+//       onClick={handleClick}
+//     >
+//       <Icon className={styles.browseDetailIcon} />
+//       <span className={styles.browseDetailBaseName}>
+//         {selectedIconBaseName}
+//       </span>
+//       <span className={styles.browseDetailStyleName}>
+//         {styleNamePart}
+//       </span>
+//       <span className={styles.browseDetailIconSuffix}>
+//         Icon
+//       </span>
+
+//       {
+//         copiedToClipboard
+//           ? (
+//             <div className={styles.browseDetailCopiedContainer}>
+//               <CheckFilledIcon className={styles.browseDetailCopiedIcon} />
+//               Copied to clipboard
+//             </div>
+//           ) : null
+//       }
+//     </div>
+//   );
+// }
